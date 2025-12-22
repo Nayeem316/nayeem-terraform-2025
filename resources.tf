9fc88add7ab3a1a4665c95aa0b088870d7f379ee
Nayeem Syed <syed.nayeem316@gmail.com>
2025-12-22 02:44:01 -0500

    Initial Terrform VPC and Subnet

---
+++ resources.tf
@@ -0,0 +1,35 @@
+# -----------------------------
+# Dev VPC
+# -----------------------------
+resource "aws_vpc" "dev_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+# -----------------------------
+# Dev Subnets
+# -----------------------------
+resource "aws_subnet" "dev_subnet_1" {
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "dev-subnet-1"
+  }
+}
+
+resource "aws_subnet" "dev_subnet_2" {
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "dev-subnet-2"
+  }
+}
